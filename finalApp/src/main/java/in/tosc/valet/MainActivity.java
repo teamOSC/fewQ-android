765babff47ffdb8833147fa2cd00acf6911c2438
Arnav Gupta <championswimmer@aokp.co>
2014-07-20 05:25:33 +0530

    add radius networks iBeacons api
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- finalApp/src/main/java/in/tosc/valet/MainActivity.java
+++ finalApp/src/main/java/in/tosc/valet/MainActivity.java
@@ -3,16 +3,30 @@ package in.tosc.valet;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.radiusnetworks.ibeacon.IBeacon;
+import com.radiusnetworks.ibeacon.IBeaconConsumer;
+import com.radiusnetworks.ibeacon.IBeaconManager;
+import com.radiusnetworks.ibeacon.MonitorNotifier;
+import com.radiusnetworks.ibeacon.RangeNotifier;
+import com.radiusnetworks.ibeacon.Region;
 
-public class MainActivity extends Activity {
+import java.util.Collection;
+
+
+public class MainActivity extends Activity implements IBeaconConsumer {
+
+    private IBeaconManager iBeaconManager = IBeaconManager.getInstanceForApplication(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iBeaconManager.bind(this);
     }
 
 
@@ -41,5 +55,48 @@ public class MainActivity extends Activity {
             break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onIBeaconServiceConnect() {
+        /*iBeaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<IBeacon> iBeacons, Region region) {
+                if (iBeacons.size() > 0) {
+                    Log.i("TOSC", "The first iBeacon I see is about " + iBeacons.iterator().next().getAccuracy() + " meters away.");
+                }
+            }
+        });
+
+        try {
+            iBeaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+        }*/
+        iBeaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                Log.i("TOSC", "didEnterRegion");
+
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                Log.i("TOSC", "didExitRegion");
+
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int state, Region region) {
+                Log.i("TOSC", "didDetermineStateForRegion");
+
+            }
+        });
+        try {
+            iBeaconManager.startMonitoringBeaconsInRegion(new Region("someId", null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
