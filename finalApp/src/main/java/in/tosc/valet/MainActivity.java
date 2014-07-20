ebdefcbc2ecf06c3e76bfbc0a8b836c0117e085c
Arnav Gupta <championswimmer@aokp.co>
2014-07-20 14:20:21 +0530

    import log
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- finalApp/src/main/java/in/tosc/valet/MainActivity.java
+++ finalApp/src/main/java/in/tosc/valet/MainActivity.java
@@ -3,6 +3,7 @@ package in.tosc.valet;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
