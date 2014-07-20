ec112114389307bfdae30c76cd2ba881f5779ad3
Arnav Gupta <championswimmer@aokp.co>
2014-07-20 15:43:36 +0530

    update transaction code
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- finalApp/src/main/java/in/tosc/valet/TransactionActivity.java
+++ finalApp/src/main/java/in/tosc/valet/TransactionActivity.java
@@ -3,14 +3,18 @@ package in.tosc.valet;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.koushikdutta.ion.Ion;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import in.tosc.valet.R;
 
 public class TransactionActivity extends Activity {
 
@@ -18,16 +22,52 @@ public class TransactionActivity extends Activity {
 
     JSONObject jsonObject = null;
 
+    LayoutInflater mInflater = null;
+
+    LinearLayout CouponsParentLayout = null;
+    LinearLayout historyParentLayout = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
 
+        CouponsParentLayout = (LinearLayout) findViewById(R.id.coupons_parent);
+        historyParentLayout = (LinearLayout) findViewById(R.id.history_parent);
+        mInflater = LayoutInflater.from(this);
         String response = getIntent().getStringExtra("data");
         try {
+            Log.d("omerjerk", "Response  = " + response);
             jsonObject = new JSONObject(response);
-            JSONArray itemsArray = jsonObject.getJSONArray("items");
+            JSONArray couponsArray = jsonObject.getJSONArray("items");
             JSONArray historyArray = jsonObject.getJSONArray("history");
+            for (int i =0; i < historyArray.length(); ++i) {
+                JSONObject mObject = historyArray.getJSONObject(i);
+                LinearLayout historyLayout = (LinearLayout) mInflater.inflate(R.layout.row_history_layout, null);
+                TextView name = (TextView) historyLayout.findViewById(R.id.name);
+                name.setText(mObject.getString("name"));
+                TextView date = (TextView) historyLayout.findViewById(R.id.date);
+                date.setText(mObject.getString("date"));
+                historyParentLayout.addView(historyLayout);
+            }
+            String token = jsonObject.getString("token");
+            TextView tokenTextView = (TextView) findViewById(R.id.token);
+            tokenTextView.setText(token);
+            for (int i = 0; i < couponsArray.length(); ++i) {
+                JSONObject mObject = couponsArray.getJSONObject(i);
+                LinearLayout couponsLayout = (LinearLayout) mInflater.inflate(R.layout.row_coupon_layout, null);
+                ImageView mImageView = (ImageView) couponsLayout.findViewById(R.id.coupon_preview);
+                TextView couponTitle = (TextView) couponsLayout.findViewById(R.id.coupon_title);
+                if (couponTitle != null)
+                    couponTitle.setText(mObject.getString("title"));
+
+                TextView couponDesc = (TextView) couponsLayout.findViewById(R.id.coupon_description);
+                couponDesc.setText(mObject.getString("text"));
+                // but for brevity, use the ImageView specific builder...
+                Ion.with(mImageView)
+                        .load(mObject.getString("image"));
+                CouponsParentLayout.addView(couponsLayout);
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
