3887339853565b9a87e811ebbaaf2150adcb3841
Umair Khan <omerjerk@gmail.com>
2014-07-20 09:29:47 +0530

    Add review Activity
    Signed-off-by: Umair Khan <omerjerk@gmail.com>
    
    Conflicts:
    	finalApp/src/main/AndroidManifest.xml
    	finalApp/src/main/java/in/tosc/valet/BeaconDetectionService.java
    	finalApp/src/main/res/values/strings.xml

--- finalApp/src/main/java/in/tosc/valet/BeaconDetectionService.java
+++ finalApp/src/main/java/in/tosc/valet/BeaconDetectionService.java
@@ -86,8 +86,9 @@ public class BeaconDetectionService extends Service implements IBeaconConsumer {
             @Override
             public void didExitRegion(Region region) {
                 Log.i("TOSC", "didExitRegion");
-                Intent tc = new Intent(getApplicationContext(), TransactionCompleteActivity.class);
-                startActivity(tc);
+                Intent intent = new Intent(BeaconDetectionService.this, ReviewActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
 
             }
 
